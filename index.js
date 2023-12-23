e51a18d42514f02a79f34fa180f892c919a6e9a5
sanjay212003 <139911764+sanjay212003@users.noreply.github.com>
2023-12-23 16:33:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
